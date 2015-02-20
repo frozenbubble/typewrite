e7af1947a81edb3880f6ec8023558cca41c5bcc8
Peter Szucs <pszucs@outlook.com>
2015-02-20 23:35:06 +0100

    Initial commit for Type write

---
+++ TypeWrite/TypeWrite.py
@@ -0,0 +1,13 @@
+import clr
+clr.AddReference("System.Drawing")
+import System.Drawing as drawing
+
+shades = {' ', '¨', '`', '.', ',', '-', '^', '/' '(', 'u', 'w', 'g', 'W', '%', '&', '#'}
+
+print('Hello world')
+
+def get_intensity(r, g, b):
+    return int((r + g + b) / 48)
+
+def get_rgb(img):
+    return 0
