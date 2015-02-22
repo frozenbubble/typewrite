c2491143f763f32835df0490da9ef4818a80220c
Peter Szucs <pszucs@outlook.com>
2015-02-22 13:09:07 +0100

    util

--- TypewriteUtil/Util.cs
+++ TypewriteUtil/Util.cs
@@ -4,26 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Net;
+using System.IO;
 
 namespace Typewrite
 {
     public class ImageUtil
     {
-        public static void GetImage()
+        public static  Bitmap GetImage(Uri url)
         {
-
+            var wc = new WebClient();
+            var data = wc.DownloadData(url);
+            var bm = Bitmap.FromStream(new MemoryStream(data)) as Bitmap;
+            
+            return bm;
         }
 
-
-
-        public void ResizeImage()
+        public static Bitmap GetImage(string path)
         {
-
+            
         }
 
-        public void GetImageAsColorArray()
+        public static Bitmap ResizeImage(Bitmap img, int height, int width)
         {
+            return new Bitmap(img, new Size(width, height));
+        }
 
+        public static Color[] GetImageAsColorArray(Image img)
+        {
+            var ic = new ImageConverter();
+            Color[] pixels = (Color[])ic.ConvertTo(img, typeof(Color[]));
+
+            return pixels;
         }
     }
 }
