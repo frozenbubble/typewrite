befde369d6c6c652ce61f850c336bc4e47cc2404
Peter Szucs <pszucs@outlook.com>
2015-04-20 10:32:42 +0200

    simple python project instead of ironpython

--- TypeWrite/TypeWrite.py
+++ TypeWrite/TypeWrite.py
@@ -1,9 +1,12 @@
-import clr
-clr.AddReferenceToFile('TypewriteUtil')
-from TypewriteUtil import ImageUtil
+from __future__ import print_function
+from PIL import Image
 
-#shades = {' ', '\'', '`', '.', ',', '-', '^', '/' '(', 'u', 'w', 'g', 'W', '%', '&', '#'}
+shades = {' ', '\'', '`', '.', ',', '-', '^', '/' '(', 'u', 'w', 'g', 'W', '%', '&', '#'}
 
+print('Hello World')
+im = Image.open("C:/Users/Peter/Pictures/Backgrounds Wallpapers HD/16699.jpg")
+pixels = im.load();
+print(im.format, im.size, im.mode)
 
-print('Hello world')
-
+for p in im.im:
+    print(p)
