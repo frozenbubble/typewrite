7202ef442acb02359a9fee60b7cee56d8dc6d338
Peter Szucs <pszucs@outlook.com>
2015-04-21 20:43:27 +0200

    working with PIL

--- TypeWrite/TypeWrite.py
+++ TypeWrite/TypeWrite.py
@@ -1,12 +1,48 @@
-from __future__ import print_function
+from math import ceil, ceil
 from PIL import Image
+import urllib3 as urllib
+import os.path
+import sys
+import re
+import io
 
-shades = {' ', '\'', '`', '.', ',', '-', '^', '/' '(', 'u', 'w', 'g', 'W', '%', '&', '#'}
 
-print('Hello World')
-im = Image.open("C:/Users/Peter/Pictures/Backgrounds Wallpapers HD/16699.jpg")
-pixels = im.load();
-print(im.format, im.size, im.mode)
+shades = ['_', '-', '^', '/', '(', 'u', 'w', 'g', 'W', '%', '&', '#']
+chars = 100
 
-for p in im.im:
-    print(p)
+if (len(sys.argv) != 2):
+    print('Usage: typewrite <filename | url>')
+    exit()
+
+arg = sys.argv[1]
+
+if (os.path.isfile(arg)):
+    im = Image.open(arg)
+
+else:
+    http = urllib.PoolManager()
+    r = http.request('GET', arg)
+    im = Image.open(io.BytesIO(r.data))
+    
+
+w,h = im.size
+im.thumbnail((chars, int(h/(w/chars))))
+w,h = im.size
+im = im.resize((w, int(h/2)))
+w,h = im.size
+
+typed = [['0' for x in range(w)] for y in range(h)]
+
+for i, p in enumerate(im.im):
+    brightness = p[0] + p[1] + p[2]
+    shade = shades[int((brightness*12)/768) - 1]
+
+    x = int(i/w)
+    y = i - int(i/w)*w
+
+    typed[x][y] = shade
+
+for row in typed:
+    r = "".join(row)
+
+    print(r)
